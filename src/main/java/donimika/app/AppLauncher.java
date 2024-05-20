dce4e6d4af1829ec17e59b582ec57c1747a0a1ec
Donimika <dominika.donatkova@gmail.com>
2024-05-20 20:51:36 +0200

    create gui and update app launcher

---
+++ src/main/java/donimika/app/AppLauncher.java
@@ -0,0 +1,16 @@
+package donimika.app;
+
+import javax.swing.*;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                // display our weather app gui
+                new WeatherAppGui().setVisible(true);
+            }
+        });
+    }
+}
